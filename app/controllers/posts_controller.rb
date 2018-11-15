e3f134924385feafd921e263ceaa5eb82f253bad
Che-Ming Lee <r05522621@ntu.edu.tw>
2018-11-16 00:44:52 +0800

    edit card border

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_author!, except: [:index, :show]
-  LIMITED_POSTS_NUMBER = 9
+  LIMITED_POSTS_NUMBER = 6
 
   def index
     if params[:page]
